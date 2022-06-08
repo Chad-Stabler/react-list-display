1946e11642909db598509680ccb1c5add4eebea3
Chad Stabler <stablerpsn@gmail.com>
2022-06-08 18:19:46 -0500

    fixing small stuff

--- src/FoodItem.js
+++ src/FoodItem.js
@@ -1,6 +1,6 @@
 export default function FoodItem({ name, serves, favorited }) {
   return (<div>
     <p>{name} is a dish that serves {serves} people</p>
-    <p>This is {favorited.name}s favorite food, he has {favorited.hair} hair and is {favorited.age} years old</p>
+    <p>This is {favorited.name}s favorite food, they have {favorited.hair} hair and are {favorited.age} years old</p>
   </div>);
 }
