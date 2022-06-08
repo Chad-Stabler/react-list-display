926b50d0b6ec2733a727617ae88b87cc88fe9598
Chad Stabler <stablerpsn@gmail.com>
2022-06-08 18:14:19 -0500

    clearing console log

--- src/RedditList.js
+++ src/RedditList.js
@@ -1,7 +1,6 @@
 import RedditItem from './RedditItem';
 
 export default function RedditList({ reddit }) {
-  console.log(reddit.data.children);
   return (<section>
     {
       reddit.data.children.map((post, i) => <RedditItem {...post.data} key={i} />)
